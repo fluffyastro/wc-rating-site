5af029275b8c9915c4a80dd3f3b96cd73cef2fdc
Artifex <krisztianka78.4@gmail.com>
2025-05-20 14:48:06 +0200

    Wawa

--- script.js
+++ script.js
@@ -1,42 +1,11 @@
-let theme_button = document.getElementById("theme")
-let theme = false
+var map = L.map('map').setView([46.65083796932437, 20.255141364694648], 13);
 
-
-//világos mód
-theme_button.addEventListener("click", function(){
-    if (theme) {
-        document.body.style.backgroundColor = "#242424"
-        document.body.style.color = "white" 
-        theme_button.innerHTML="Világos mód"
-        theme = false
-    }
-    else{
-        document.body.style.backgroundColor = "white"
-        document.body.style.color = "black"
-        theme_button.innerHTML="Sötét mód"
-        theme = true
-    }
-});
-var element = document.getElementById('map');
-
-// Height has to be set. You can do this in CSS too.
-element.style = 'height:500px;';
-
-// Create Leaflet map on map element.
-var map = L.map(element);
-
-// Add OSM tile layer to the Leaflet map.
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Target's GPS coordinates.
-var target = L.latLng('46.650994637726946','20.25513063585885');
+L.marker([46.65083796932437, 20.255141364694648]).addTo(map)
+    .bindPopup('Szent Erszébet alsós wc<hr>Valaki Vildikó   2025 május 10 <br> <br>Blast to the past 2/10');
 
-// Set map's center to target with zoom 14.
-map.setView(target, 14);
-
-
-L.marker(target).addTo(map).on('click', function() {
-    
-});
+L.marker([46.65083796932437, 20.255141364694648]).addTo(map)
+    .bindPopup('Szent Erszébet alsós wc<hr>Valaki Vildikó   2025 május 10 <br> <br>Blast to the past 2/10');
