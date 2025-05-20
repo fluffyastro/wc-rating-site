94022182518d0d3a2ad3273683e1f83bc061fd02
Artifex <krisztianka78.4@gmail.com>
2025-05-20 14:53:11 +0200

    sasasaas

--- script.js
+++ script.js
@@ -5,7 +5,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.marker([46.65083796932437, 20.255141364694648]).addTo(map)
-    .bindPopup('Szent Erszébet alsós wc<hr>Valaki Vildikó   2025 május 10 <br> <br>Blast to the past 2/10');
-
-L.marker([46.65083796932437, 20.255141364694648]).addTo(map)
-    .bindPopup('Szent Erszébet alsós wc<hr>Valaki Vildikó   2025 május 10 <br> <br>Blast to the past 2/10');
+    .bindPopup('Szent Erszébet alsós wc<hr>Valaki Vildikó   2025 május 10 <br> <br>Blast to the past 2/10<hr>Joe Mama 2025 május 24 <br> <br>Nagyon tiszta 8/10');
